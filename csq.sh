322c5236b70a29b2cf6be8e6185ee14db583f9c2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-12-22 11:59:28 +0100

    new shell scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

---
+++ csq.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# RF=... - specify red func to apply
+# GF=... - specify green func to apply
+# BF=... - specify blue func to apply
+if [ -z "$RF" ]
+then
+  RF='if(x1<.95,if(x1<.33333,x1*3,1),1-2*(x1-.95))'
+fi
+if [ -z "$GF" ]
+then
+  GF='if(x1<.95,if(x1<.33333,0,if(x1>.66667,1,(x1-.33333)*3)),1-2*(x1-.95))'
+fi
+if [ -z "$BF" ]
+then
+  BF='if(x1<.66667,0,(x1-.66667)*3)'
+fi
+RC=1 GC=1 BC=1 RLO=1 RHI=1 GLO=1 GHI=1 BLO=1 BHI=1 NA=1 INF=20 PQ=1 HINT=1 MODE=veryslow RF="$RF" GF="$GF" BF="$BF" csqconv "$@"
---
+++ csqbw.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# F=... - specify GS func to apply
+if [ -z "$F" ]
+then
+  F="(1-x1)"
+fi
+MODE=veryslow HINT=1 INF=20 PQ=1 OGS=1 NA=1 GSR=1 GSG=0 GSB=0 RLO=1 RHI=1 RF="$F" RC=1 RGA=1.41 csqconv "$@"
