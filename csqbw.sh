d74e831ef9e271890644bff60ef1c38b40d75bee
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2021-03-13 16:15:15 +0100

    Update default scripts
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- csqbw.sh
+++ csqbw.sh
@@ -4,4 +4,4 @@ if [ -z "$F" ]
 then
   F="(1-x1)"
 fi
-MODE=veryslow HINT=1 INF=20 PQ=1 OGS=1 NA=1 GSR=1 GSG=0 GSB=0 RLO=1 RHI=1 RF="$F" RC=1 RGA=1.41 csqconv "$@"
+MODE=veryslow HINT=1 INF=20 PQ=1 OGS=1 NA=1 GSR=1 GSG=0 GSB=0 RLO=1 RHI=1 RF="$F" RC=1 RGA=1.41 MF=100 CRF=14 csqconv "$@"
